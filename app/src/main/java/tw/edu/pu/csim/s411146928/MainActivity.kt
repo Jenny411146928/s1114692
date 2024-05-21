3e106df74245995fca81616b8116fbf46cdec474
Jenny411146928 <jenny1211jenny@gmail.com>
2024-05-21 23:04:28 +0800

    v3_李盈蓁

--- app/src/main/java/tw/edu/pu/csim/s411146928/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s411146928/MainActivity.kt
@@ -4,12 +4,17 @@ package tw.edu.pu.csim.s411146928
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -20,12 +25,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -59,11 +68,44 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun FirstScreen(navController: NavController) {
     val context = LocalContext.current
+    var showTitle by remember { mutableStateOf(true) }
+    var expanded by remember { mutableStateOf(true) }
+    var currentImage by remember { mutableStateOf(R.drawable.service)}
 
     Column(modifier = Modifier)
     {
+        Text(text = if (currentImage == R.drawable.service)"瑪利亞基金會服務總覽" else "關於App作者", style = TextStyle(color = Color.Blue))
 
-        Text("簡介", style = TextStyle(color = Color.Blue)) //顏色改藍色
+        var alpha by remember {
+            mutableStateOf(1f)
+        }
+        LaunchedEffect(currentImage){
+            val animatable = androidx.compose.animation.core.Animatable(0f)
+            animatable.animateTo(1f, animationSpec = tween(5000)){
+                alpha = value
+            }
+        }
+
+        Image(painter = painterResource(currentImage), contentDescription = "service", modifier = Modifier
+            .clip(RectangleShape)
+            .fillMaxWidth()
+            .height(if(expanded)600.dp else 400.dp)
+            .clickable { expanded = !expanded }
+            .alpha(alpha)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            showTitle = !showTitle
+            currentImage = if (currentImage == R.drawable.service) {
+                R.drawable.j
+            } else {
+                R.drawable.service
+            }
+        }) {
+            Text(text = if (currentImage == R.drawable.service) "作者：資管系李盈蓁" else "服務總覽")
+        }
     }
 }
 
@@ -73,11 +115,11 @@ fun SecondScreen(navController: NavController) {
     val context = LocalContext.current
 
     Column(modifier = Modifier)
+
     {
-
         Text("主要機構", style = TextStyle(color = Color.Red)) //顏色改紅色
-
     }
+
 }
 
 
@@ -95,12 +137,10 @@ fun Main() {
         TopAppBar(
             title = { Text(text = "")
             Image(
-                painter = painterResource(
-                    id = R.drawable.maria),
-                contentDescription = null,
-                modifier = Modifier
+                painter = painterResource(id = R.drawable.maria), contentDescription = null, modifier = Modifier
                     .height(50.dp),
             )
+
     },
             actions = {
                 IconButton(
